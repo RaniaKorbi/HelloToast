83f4b4f246fbfbf59c98c26f8eb4cb2043c2ec77
Rania Korbi <raniakorbi56@gmail.com>
2021-10-15 19:08:32 +0100

    Second Commit

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int TEXT_REQUEST = 1;
     private int mCount = 0;
     private TextView mShowCount;
+    public static final String EXTRA_MESSAGE =
+            "com.example.hellotoast.extra.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(this, R.string.toast_message,
                 Toast.LENGTH_SHORT);
         toast.show();
+        Intent intent = new Intent(this, HelloToast.class);
+        String message =mShowCount.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 
     public void countUp(View view) {
